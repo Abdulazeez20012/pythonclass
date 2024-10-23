aa9cfd0c244cad9bdc49cd597124164020622039
Abdulazeez20012 <abdulazeezmuhammad855@gmail.com>
2024-10-23 21:39:09 +0100

    update

--- Chapter 2 pyhon/Feetconverter.py
+++ Chapter 2 pyhon/Feetconverter.py
@@ -1,6 +1,4 @@
-print("hello  Let's convert feet to meters.")
-
 feet = float(input("Please enter the length in feet: "))
 meters = feet * 0.305
+
 print(f"{feet} feet is equal to {meters:.2f} meters.")
-print("Conversion complete! Thank you for using our converter.")
